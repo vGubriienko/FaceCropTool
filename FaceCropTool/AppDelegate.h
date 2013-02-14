6ee863c85a12338be2190bd3376d4bdc414b6b87
Viktor Gubriienko <viktor@micro-b.net>
2013-02-14 15:21:24 +0200

    Initial Commit

---
+++ FaceCropTool/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FaceCropTool
+//
+//  Created by Viktor Gubriienko on 14.02.13.
+//  Copyright (c) 2013 Viktor Gubriienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ FaceCropTool/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FaceCropTool
+//
+//  Created by Viktor Gubriienko on 14.02.13.
+//  Copyright (c) 2013 Viktor Gubriienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
